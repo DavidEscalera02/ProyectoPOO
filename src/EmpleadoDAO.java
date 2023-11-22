e659f7d622703907840ed3f3c978bfe1b688957a
DavidEscalera02 <146364358+DavidEscalera02@users.noreply.github.com>
2023-11-22 13:20:34 -0400

    Update EmpleadoDAO.java

--- src/EmpleadoDAO.java
+++ src/EmpleadoDAO.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class EmpleadoDAO {
     public static void crearEmpleadoDB(Empleado empleado) {
