be76e323d4894ebc4ee7508d7c4b627d4a0242ec
David <descalera484@gmail.com>
2023-11-22 16:36:22 -0400

    Terminado Proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    }
+
+            Scanner sc = new Scanner(System.in);
+            int opcion = 0;
+
+
+            do {
+                System.out.println("-------MENU PRINCIPAL------");
+                System.out.println("1-  Clientes");
+                System.out.println("2-  Empleados");
+                System.out.println("3-  almacen");
+                System.out.println("4-  Salir");
+                System.out.print("Seleccione una opcion: ");
+
+                opcion = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcion) {
+                    case 1:
+                        ClienteMenu.menuClientes();
+                        break;
+                    case 2:
+                        EmpleadoMenu.menuEmpleado();
+                        break;
+                    case 3:
+                        ALmacenMenu.menuAlmacen();
+                        break;
+                    case 4:
+                        System.out.println("Saliendo del programa. ¡Que tenga un buen dia...!");
+                        break;
+                    default:
+                        System.out.println("Opcion no valida... Por favor, seleccione una opcion valida.");
+                }
+
+            } while (opcion != 4);{
+                sc.close();
+            }
+        }
 }
